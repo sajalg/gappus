c28b0ce8bb6559ea04cbcc53dfde812aa31dc4dc
sajalg <sajalg2001@gmail.com>
2018-04-01 18:21:09 +0530

    Sajal is best

---
+++ src/main/java/com/cs/demos/sampl.java
@@ -0,0 +1,72 @@
+package com.cs.demos;
+
+import java.io.IOException;
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.StringTokenizer;
+
+public class sampl {
+	
+	public static void main(String[]args) throws IOException{
+StringTokenizer st=new StringTokenizer("sajal is goy-al "," ");
+while(st.hasMoreTokens()){
+	System.out.println(st.nextToken("-"));
+}
+		double f1 = .0;
+	    for (int i = 1; i <= 11; i++) {
+	        f1 += .1;
+	    }
+	 
+	    //Method 2
+	    double f2 = .1 * 11;
+	 
+	    System.out.println("f1 = " + f1);
+	    System.out.println("f2 = " + f2);
+
+
+
+	
+	}
+}
+
+
+class g implements UncaughtExceptionHandler{
+	@Override
+	public void uncaughtException(Thread t,Throwable k){
+		
+	}
+}
+
+class k extends Thread
+{
+	k(String name){
+		super(name);
+	}
+	
+	@Override
+	public void start(){
+		
+		run();
+	}
+	
+	@Override
+	public void run(){
+		System.out.println(Thread.currentThread().getState());
+	System.out.println("sajal is best");	
+	}
+}
+class sam1 implements Runnable{
+	int a=10;
+	public sam1(int m){
+		this.a=m;
+	}
+	public void run(){
+		int m=10;
+		System.out.println("sajal is best");
+	}
+}
+
+@FunctionalInterface
+interface Supplier<Integer>{
+	public Integer get();
+}
+
