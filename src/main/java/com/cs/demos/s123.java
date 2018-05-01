3128fe30a0a9820f923e57086a9a7195f3e5c429
sajalg <sajalg2001@gmail.com>
2018-05-01 13:19:09 +0530

    sam

---
+++ src/main/java/com/cs/demos/s123.java
@@ -0,0 +1,14 @@
+package com.cs.demos;
+
+public class s123 {
+
+	public s123() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
